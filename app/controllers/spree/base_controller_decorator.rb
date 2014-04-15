5bc32bc71ace4ec1da0094d7f1d2bed407dd330e
Matthew Fenelon <matt@fnln.me>
2014-04-15 14:14:40 +0100

    The default subdomain is now used if the subdomain of the request is not known.

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -1,33 +1,27 @@
 # encoding: utf-8
 
 Spree::BaseController.class_eval do
-  before_filter :get_subdomain
-
-  def current_subdomain
-    @current_subdomain = request.subdomain
-  end
+  before_filter :set_language_and_currency_based_on_subdomain
 
   private
 
-  def get_subdomain
-    req_subdomain = request.subdomain
-    subdomain = if req_subdomain.present?
-      Spree::Subdomain.find_by_name(req_subdomain)
-    else
-      Spree::Subdomain.find_by_name(Spree::Config[:default_subdomain])
+    def set_language_and_currency_based_on_subdomain
+      req_subdomain = request.subdomain
+      subdomain = Spree::Subdomain.find_by_name(req_subdomain)
+
+      if subdomain.nil?
+        subdomain = Spree::Subdomain.find_by_name(Spree::Config[:default_subdomain])
+      end
+
+      session[:subdomain] = subdomain.name
+
+      # Using the Money gem, we just need to find and store the currency ISO
+      currency = supported_currencies.find { |currency| currency.iso_code == subdomain.currency_iso}
+      session[:currency] = subdomain.currency_iso
+
+      # And set the locale
+      if I18n.available_locales.map(&:to_s).include?(subdomain.locale)
+        session[:locale] = I18n.locale = subdomain.locale
+      end
     end
-
-    return if subdomain.nil?
-
-    session[:subdomain] = subdomain.name 
-    
-    # Using the Money gem, we just need to find and store the currency ISO
-    currency = supported_currencies.find { |currency| currency.iso_code == subdomain.currency_iso}
-    session[:currency] = subdomain.currency_iso
-
-    # And set the locale
-    if I18n.available_locales.map(&:to_s).include?(subdomain.locale)
-      session[:locale] = I18n.locale = subdomain.locale
-    end
-  end
 end
